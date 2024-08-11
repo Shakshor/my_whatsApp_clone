67f738c0f39912c84f7e3e4337f95cce3310fb55
shakshor <shakshor584@gmail.com>
2024-08-11 07:58:41 +0600

    created app bar

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: ResponsiveLayout(
+      home: const ResponsiveLayout(
         mobileScreenLayout: MobileScreenLayout(),
         webScreenLayout: WebScreenLayout(),
       ),
--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:whats_app_clone/colors.dart';
 
 class MobileScreenLayout extends StatelessWidget{
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Center(
-          child: Text('This is mobile')
+    return  DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: appBarColor,
+          title: const Text(
+              'WhatsApp',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+            ),
+            IconButton(
+                onPressed: (){},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+            ),
+          ],
+          bottom: const TabBar(
+            dividerColor: Colors.transparent,
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(
+                text: 'CHATS',
+              ),
+              Tab(
+                text: 'STATUS',
+              ),
+              Tab(
+                text: 'CALLS',
+              ),
+            ],
+
+          ),
+        ),
+
+        body: Center(
+            child: Text('This is mobile')
+        ),
       ),
     );
   }
