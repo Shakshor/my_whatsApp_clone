5a12de178fb535949b49389987aa0823dd091cbd
shakshor <shakshor584@gmail.com>
2024-08-13 23:18:28 +0600

    web_screen_layout created

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:whats_app_clone/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget{
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Center(
-          child: Text('This is web'),
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // web_profile_bar
+                  // web_search_bar
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: size.width * 0.75,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/backgroundImage.png'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
